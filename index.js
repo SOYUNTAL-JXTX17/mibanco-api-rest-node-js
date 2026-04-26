8011b076379a59708e465411827c3183bc4e8b30
JXTX Developer <a25jairoencyll@iesluiscarrillodesotomayor.org>
2026-04-26 15:15:33 +0200

    Fix PORT assignment to include fallback for 8080

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ app.get('/users', (req, res) => {
     });
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000 || 8080;
 
 app.listen(PORT, '0.0.0.0', () => {
     console.log(`Servidor corriendo en puerto ${PORT}`);
