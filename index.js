275f5adfd09bb8832b45cfc73cf7e9c42889e981
JXTX Developer <a25jairoencyll@iesluiscarrillodesotomayor.org>
2026-04-26 15:19:59 +0200

    Fix PORT assignment and update server listener syntax

--- index.js
+++ index.js
@@ -25,8 +25,8 @@ app.get('/users', (req, res) => {
     });
 });
 
-const PORT = process.env.PORT || 3000 || 8080;
+const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, '0.0.0.0', () => {
+app.listen(PORT, '0.0.0.0', function () {
     console.log(`Servidor corriendo en puerto ${PORT}`);
 });
