a0535450cf165189c558d1044efe9d0d6e82078d
JXTX Developer <a25jairoencyll@iesluiscarrillodesotomayor.org>
2026-04-26 16:24:06 +0200

    Refactor MySQL connection to use environment variables

--- index.js
+++ index.js
@@ -7,7 +7,16 @@ console.log('DB_PORT:', process.env.DB_PORT);
 console.log('DB_USER:', process.env.DB_USER);
 console.log('DB_DATABASE:', process.env.DB_DATABASE);
 
-const pool = mysql.createPool(process.env.MYSQL_URL);
+const pool = mysql.createPool({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+    port: process.env.DB_PORT,
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0
+});
 
 pool.getConnection((err, connection) => {
     if (err) {
