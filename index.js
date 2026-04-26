0f4e7339bfd98d484aefe199857658d4f6989c0f
JXTX Developer <a25jairoencyll@iesluiscarrillodesotomayor.org>
2026-04-26 16:43:47 +0200

    Use environment variable for server port

--- index.js
+++ index.js
@@ -37,6 +37,7 @@ app.get('/users', (req, res) => {
     });
 });
 
-app.listen(3000, '0.0.0.0', () => {
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, '0.0.0.0', () => {
     console.log(`Servidor corriendo en puerto ${PORT} ✅`);
 });
