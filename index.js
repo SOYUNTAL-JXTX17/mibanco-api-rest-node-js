fd67cae30ed85a065e7e3be9c299be0e09aea20d
JXTX Developer <a25jairoencyll@iesluiscarrillodesotomayor.org>
2026-04-26 17:10:51 +0200

    Change server port to 3000

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ app.get('/users', (req, res) => {
     });
 });
 
-const PORT = process.env.PORT || 8080;
+const PORT = 3000;
 app.listen(PORT, '0.0.0.0', () => {
     console.log(`Servidor corriendo en puerto ${PORT} ✅`);
 });
